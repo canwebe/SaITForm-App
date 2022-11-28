4b7da7cfa83e41c3a5628fd10122f093095ecfac
devRabbani <dev.golamrabbani@gmail.com>
2022-11-29 00:17:21 +0530

    adding dynamic import to some component

--- components/adminComp/adminContent/index.js
+++ components/adminComp/adminContent/index.js
@@ -1,6 +1,7 @@
+import dynamic from 'next/dynamic'
 import { MdOutlineArrowDropDown, MdOutlineArrowDropUp } from 'react-icons/md'
-import Pagination from '../pagination'
 import s from './adminContent.module.css'
+const Pagination = dynamic(() => import('../pagination'))
 
 export default function AdminContent({
   getTableProps,
